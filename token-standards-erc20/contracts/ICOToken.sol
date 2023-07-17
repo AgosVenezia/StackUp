9c5459fd2a9416c5631ce79f0b46e7a834697c84
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2023-07-17 13:01:33 -0300

    Token Standards Masterclass - ERC20 - ERC20 Fundamentals

--- token-standards-erc20/contracts/ICOToken.sol
+++ token-standards-erc20/contracts/ICOToken.sol
@@ -9,9 +9,21 @@ contract MyICO is ERC20 {
     uint256 public startTime;
     uint256 public constant SALE_DURATION = 1 days;
 
-    // insert contructor() function
+    constructor(uint256 _amount) ERC20("Aguito", "ICO") {
+        owner = msg.sender;
+        _mint(msg.sender, _amount);
+        startTime = block.timestamp;
+    }
 
-    // insert ownerMint() function
+    function ownerMint(uint256 _amount) external {
+        require(msg.sender == owner, "Not the owner");
+        _mint(owner, _amount);
+    }
 
-    // insert buyTokens() function
+    function buyTokens(uint256 _amount) external payable {
+        require(block.timestamp <= startTime + SALE_DURATION, "Sale has expired");
+        require(msg.value >= 1 ether, "Minimum amount of ETH sent is 1");
+        _mint(msg.sender, _amount);
+    }
+
 }
